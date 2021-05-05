10e41c54b9390259a9c2074c8419d2fe74a9e5f7
Hudson <dhudsmith@gmail.com>
2021-05-04 21:44:39 -0400

    document propagator classes

--- qmsolve/time_evolve.py
+++ qmsolve/time_evolve.py
@@ -32,18 +32,35 @@ class Propagator(ABC):
 
 
 class VisscherPropagator(Propagator):
+    """
+    Time evolution based on explicit time integration using Visscher's method.
+    "A fast explicit algorithm for the time‐dependent Schrödinger equation"
+    Computers in Physics 5, 596 (1991); https://doi.org/10.1063/1.168415.
+    """
     def __init__(self, hamiltonian: Hamiltonian,
                  init_state: Callable[[np.ndarray], np.ndarray],
                  dt: float,
                  sample_interval: int):
+        """
+        Construct a Visscher Propagator. This Propagator takes an additional `sample_interval`
+        argument to allow sampling of wave functions at longer time intervals than used for solving.
+        used for integrating the TDSE.
+        :param hamiltonian: The hamiltonian for the system.
+        :param init_state: The initial state function
+        :param dt: The time step to use when integrating the TDSE
+        :param sample_interval: The number of time steps between output wave functions.
+        """
         super().__init__(hamiltonian, init_state, dt)
 
         self.sample_interval = sample_interval
         self.hmat = self.hamiltonian.hamiltonian_matrix()
 
     def evolve(self) -> Generator[np.ndarray, None, None]:
-        # compute initial conditions
-        # take a half time step forward to compute initial imaginary part
+        """
+        Generate the time evolved wave function.
+        :return: a generator object which can be used to fetch the wave function values
+        at successive timesteps.
+        """
         psi0 = self.init_state_grid.T.squeeze()
         psi1 = psi0 + 1.j * (self.dt/2) * self.hmat @ psi0
         R = psi0.real
@@ -67,11 +84,24 @@ class VisscherPropagator(Propagator):
 
 
 class DiagonalizationPropagator(Propagator):
+    """
+    Time evolution based on exact diagonalization of the Hamiltonian.
+    """
     def __init__(self, hamiltonian: Hamiltonian,
                  init_state: Callable[[np.ndarray], np.ndarray],
                  dt: float,
                  solver: Solver,
                  k: int):
+        """
+        Construct a diagonalization-based propagator object. As this
+        method relies upon diagonalization of the hamiltonian, you must pass an
+        additional solver object.
+        :param hamiltonian: The hamiltonian for the system.
+        :param init_state: The initial state function.
+        :param dt: The time step at which to sample the time-dependent wave function.
+        :param solver: The solver object for diagonalizing the hamiltonian
+        :param k: The number of eigenstates to use when approximating the exact propagator.
+        """
         super().__init__(hamiltonian, init_state, dt)
 
         self.solver = solver
@@ -79,7 +109,12 @@ class DiagonalizationPropagator(Propagator):
         self.eigs, self.vecs = self.solver.eigsys(self.hamiltonian, self.k)
 
     def evolve(self) -> Generator[np.ndarray, None, None]:
-        # compute the time-dependent wave function
+        """
+        Generate the time evolved wave function.
+        :return: a generator object which can be used to fetch the wave function values
+        at successive timesteps.
+        """
+
         coeffs = self.vecs @ self.init_state_grid.T
 
         # loop over time
